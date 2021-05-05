810868fd375b7ea2dee820da966353cbad180f1e
Jean <jean.bourgarel@epitech.eu>
2021-05-05 18:50:30 +0200

    added click listener on card

--- app/src/main/java/com/example/myundercover/adapters/PlayerCardAdapter.kt
+++ app/src/main/java/com/example/myundercover/adapters/PlayerCardAdapter.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myundercover.PlayerCard
 import com.example.myundercover.R
@@ -19,13 +20,12 @@ class PlayerCardAdapter(val playerCards: ArrayList<PlayerCard>, val context: Con
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlayerCardHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val cellForRow = layoutInflater.inflate(R.layout.grid_layout_list_item, parent, false)
-        return PlayerCardHolder(cellForRow)
+        return PlayerCardHolder(cellForRow, context)
     }
 
     override fun onBindViewHolder(holder: PlayerCardHolder, position: Int) {
         val playerCard = playerCards[position]
-
-
+        
         holder.icon.setImageResource(playerCard.icon!!)
         holder.name.text = playerCard.name
     }
@@ -33,7 +33,10 @@ class PlayerCardAdapter(val playerCards: ArrayList<PlayerCard>, val context: Con
 
 }
 
-class PlayerCardHolder(val view: View): RecyclerView.ViewHolder(view) {
+class PlayerCardHolder(val view: View, context: Context): RecyclerView.ViewHolder(view) {
+    val click = itemView.setOnClickListener {
+        Toast.makeText(context, "Select name", Toast.LENGTH_SHORT).show()
+    }
     var icon = itemView.findViewById<ImageView>(R.id.icon_player_card)
     var name = itemView.findViewById<TextView>(R.id.player_name)
 }
